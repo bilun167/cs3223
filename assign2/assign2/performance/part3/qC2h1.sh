70c79becf141ea2fb9e71658bcfd004fb9d56703
Tai Huynh <bilun167@gmail.com>
2014-04-05 23:29:04 +0800

    benchmark

---
+++ assign2/assign2/performance/part3/qC2h1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# Experiment A: Measure performance of PostgreSQL's hash join as X is varied
+
+### load data into R and S
+# sh ./loaddata.sh 100000 10
+
+### Run query Q1 for different values of selection constant X
+for i in 12500 25000 37500 50000 75000
+do
+psql -e -v x=$i assign2 <<EOF
+	SET enable_nestloop TO FALSE;
+	SET enable_mergejoin TO FALSE;
+	SET work_mem TO 64; 
+	SET hash_method TO 1;
+	SET bitvector_size TO 512;
+	-- measure query running time
+	\timing on
+	SELECT COUNT(*) FROM S WHERE EXISTS ( SELECT * FROM R WHERE R.A = S.A AND R.B <= :x);
+	\timing off
+	-- examine query plan
+ 	EXPLAIN ANALYZE SELECT COUNT(*) FROM S WHERE EXISTS ( SELECT * FROM R WHERE R.A = S.A AND R.B <= :x);
+	\q
+EOF
+done
+
+
---
+++ assign2/assign2/performance/part3/qD3h1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# Experiment B: Measure performance of PostgreSQL's hash join as work_mem is varied
+
+### load data into R and S
+# sh ./loaddata.sh 100000 10
+
+### Run query Q1 for different values of work_mem
+for i in 128 256 512 1024 2048 4096 
+do
+psql -e -v wm=$i assign2 <<EOF
+	SET enable_nestloop TO FALSE;
+	SET enable_mergejoin TO FALSE;
+	SET work_mem TO 64; 
+    SET bitvector_size to :wm;
+    SET hash_method to 1;
+	-- measure query running time
+	\timing on
+	SELECT COUNT(*) FROM S WHERE NOT EXISTS (SELECT * FROM R WHERE R.A = S.A AND R.B <= 50000);
+	\timing off
+	-- examine query plan
+	EXPLAIN ANALYZE SELECT COUNT(*) FROM S WHERE NOT EXISTS (SELECT * FROM R WHERE R.A = S.A AND R.B <= 50000);  
+	\q
+EOF
+done
+
+
